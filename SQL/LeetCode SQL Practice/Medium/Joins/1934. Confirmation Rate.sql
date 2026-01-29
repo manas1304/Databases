0bdc2b0a519b50657d2e1905d8c9be721dd4aaf7
Manas Shukla <manasshukla01304@gmail.com>
2026-01-29 17:08:27 +0530

    Leetcode 1934 - Almost Self Done

---
+++ SQL/LeetCode SQL Practice/Medium/Joins/1934. Confirmation Rate.sql
@@ -0,0 +1,14 @@
+-- Link -- https://leetcode.com/problems/confirmation-rate/description/
+-- Question:
+-- The confirmation rate of a user is the number of 'confirmed' messages divided by the total number of requested confirmation messages. 
+-- The confirmation rate of a user that did not request any confirmation messages is 0. Round the confirmation rate to two decimal places.
+-- Write a solution to find the confirmation rate of each user.
+-- Return the result table in any order.
+
+select s.user_id,
+round(
+    avg(case when c.action = 'confirmed' then 1 else 0 end), 2
+) as confirmation_rate
+from Signups s
+left join Confirmations c on s.user_id = c.user_id
+group by s.user_id;
